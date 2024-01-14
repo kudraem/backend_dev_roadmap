f3259027cfced3789217b5d2ab5085596282018f
Nikita Savin <klaster26@mail.ru>
2024-01-14 18:35:28 +0300

    fix: сокращение и оптимизация кода

--- projects/telegram_notification_service/sprint_1/tasks/task_1/solution/script.py
+++ projects/telegram_notification_service/sprint_1/tasks/task_1/solution/script.py
@@ -4,7 +4,6 @@ import requests
 class Todo(requests.Session):
     def __init__(self):
         super().__init__()
-        self.todo_list = None
         self.max_redirects = 5
         self.url = 'https://dummyjson.com/todos'
         self.limit = None
@@ -13,49 +12,10 @@ class Todo(requests.Session):
         self.headers = {'User-Agent': 'Python-Study-App/1.0.0',
                         'Content-Type': 'application/json'}
 
-    def get(self, delimiter=5):
-        """
-        Метод возвращает указанное в качестве аргумента количество дел
-        (значение по умолчанию - 5)
-        """
-        self.params.update(limit=delimiter)
+    def get(self, url='https://dummyjson.com/todos'):
         try:
-            response = super().get(self.url, params=self.params,
+            response = super().get(url, params=self.params,
                                    headers=self.headers)
-            response.raise_for_status()
-        except requests.TooManyRedirects:
-            return 'Sorry, too many redirects'
-        except requests.HTTPError as err:
-            return f'HTTPError is occured, and it is {err}'
-        except requests.Timeout:
-            return 'Timeout error. Try again later.'
-        except requests.ConnectionError:
-            return 'Connection is lost, try again later.'
-        else:
-            try:
-                self.todo_list = response.json()
-                return self.todo_list['todos']
-            except requests.JSONDecodeError:
-                return 'Incoming JSON is invalid'
-
-    def next(self, skip=5, delimiter=5):
-        """
-        Метод позволяет пропустить skip элементов в списке дел
-        и получить следующие delimiter дел.
-        Значение skip по-умолчанию равно 5.
-        Значение delimiter по-умолчанию - 5.
-        """
-        self.skip += skip
-        self.params.update(skip=self.skip)
-        return self.get(delimiter)
-
-    def id(self, event_id):
-        """Метод позволяет получить из списка конкретное дело
-        с заданным пользователем id.
-        """
-        url = f'{self.url}/{event_id}'
-        try:
-            response = super().get(url, headers=self.headers)
             response.raise_for_status()
         except requests.TooManyRedirects:
             return 'Sorry, too many redirects'
@@ -71,11 +31,27 @@ class Todo(requests.Session):
             except requests.JSONDecodeError:
                 return 'Incoming JSON is invalid'
 
+    def list(self, delimiter=5, step=0):
+        """
+        Метод позволяет пропустить skip элементов в списке дел
+        и получить следующие delimiter дел.
+        Значение skip по-умолчанию равно 0.
+        Значение delimiter по-умолчанию - 5.
+        """
+        self.params.update(limit=delimiter, skip=step)
+        return self.get()['todos']
+
+    def id(self, event_id):
+        """Метод позволяет получить из списка конкретное дело
+        с заданным пользователем id.
+        """
+        return self.get(f'{self.url}/{event_id}')
+
 
 new = Todo()
-response = new.get(10)
+response = new.list(10)
 assert response[1]['id'] == 2
-response = new.next(5, 10)
+response = new.list(10, 5)
 assert response[6]['id'] == 12
 response = new.id(5)
 assert response['todo'] == "Solve a Rubik's cube"
