dd88975986a394064039d72eb51a11f97d249c30
Nikita Savin <klaster26@mail.ru>
2024-01-17 15:12:09 +0300

    feat: добавить реализацию метода enlist в класс Todo

--- projects/telegram_notification_service/sprint_1/tasks/task_1/solution/todo/todo/script.py
+++ projects/telegram_notification_service/sprint_1/tasks/task_1/solution/todo/todo/script.py
@@ -21,25 +21,52 @@ def catch_exception(request):
 
 
 class DummyJsonApi(requests.Session):
-    def __init__(self):
+    def __init__(self, domain):
         super().__init__()
         self.max_redirects = 5
-        self.domain = 'https://dummyjson.com'
+        self.domain = domain
         self.headers = {'User-Agent': 'Python-Study-App/1.0.0',
                         'Content-Type': 'application/json'}
 
     def get(self, path='/todos', delimiter=None, step=None):
         params = {'limit': delimiter, 'skip': step}
-        return catch_exception(super().get(self.domain + path, params=params,
-                                   headers=self.headers))
+        return catch_exception(super().get(self.domain + path,
+                                           params=params,
+                                           headers=self.headers))
 
     def post(self, request_body, path='/todos'):
-        return catch_exception(super().post(self.domain + path, json=request_body,
-                                    headers=self.headers))
+        return catch_exception(super().post(self.domain + path,
+                                            json=request_body,
+                                            headers=self.headers))
 
     def patch(self, request_body, path='/todos'):
-        return catch_exception(super().patch(self.domain + path, json=request_body,
-                                     headers=self.headers))
+        return catch_exception(super().patch(self.domain + path,
+                                             json=request_body,
+                                             headers=self.headers))
 
     def delete(self, path='/todos'):
-        return catch_exception(super().delete(self.domain + path, headers=self.headers))
+        return catch_exception(super().delete(self.domain + path,
+                                              headers=self.headers))
+
+
+class Todo:
+    def __init__(self):
+        self.dummyjson = DummyJsonApi('https://dummyjson.com')
+
+    def enlist(self, delimiter=5, step=0):
+        """
+        list(self, delimiter=5, step=0)
+
+        Метод позволяет пропустить step элементов в списке дел
+        и получить следующие delimiter дел.
+        Значение step по-умолчанию - 0.
+        Значение delimiter по-умолчанию - 5.
+        """
+        todoes_dict = self.dummyjson.get(delimiter=delimiter, step=step)
+        return todoes_dict.get('todos')
+
+
+new = Todo()
+response = new.enlist(10)
+assert response[1]['id'] == 2
+print('Tests passed')
