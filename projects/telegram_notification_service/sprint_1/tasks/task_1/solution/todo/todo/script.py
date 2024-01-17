f1316fdc5cb110f4037d2a0aa5bbf6b5055e1c13
Nikita Savin <klaster26@mail.ru>
2024-01-17 16:31:11 +0300

    fix: выделить тесты в отдельный скрипт

--- projects/telegram_notification_service/sprint_1/tasks/task_1/solution/todo/todo/script.py
+++ projects/telegram_notification_service/sprint_1/tasks/task_1/solution/todo/todo/script.py
@@ -142,23 +142,3 @@ class Todo:
         """
         path = f'/{task_id}'
         return self.dummyjson.delete(path=path)
-
-
-new = Todo()
-response = new.enlist(10)
-assert response[1]['id'] == 2
-response = new.enlist(10, 5)
-assert response[6]['id'] == 12
-response = new.id(5)
-assert response['todo'] == "Solve a Rubik's cube"
-response = new.random()
-assert response['id'] > 0
-response = new.user(5)
-assert response[0]['userId'] == 5
-response = new.add('Send another request to dummyjson.com', 31, False)
-assert response['id'] == 151
-response = new.update(1, False)
-assert response['completed'] is False
-response = new.remove(1)
-assert response['isDeleted'] is True
-print('Tests passed')
