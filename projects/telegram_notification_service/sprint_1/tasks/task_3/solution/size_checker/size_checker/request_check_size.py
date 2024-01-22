da26aa99416a0a5cc2b06277ce92537f0a9937c3
Nikita Savin <klaster26@mail.ru>
2024-01-22 22:58:33 +0300

    feat: добавить тесты поднятия исключения TooMany..
    Redirects.
    Также реализована альтернативная функция для конвертации
    размера ответа.
    Теперь функция возвращает только размер ответа в байтах (класс 'int'),
    но тесты изменены для проверки функциональности как в случае возвращения
    просто числа байта, так и в случае получения данных в человекочитаемом
    формате.

--- projects/telegram_notification_service/sprint_1/tasks/task_3/solution/size_checker/size_checker/request_check_size.py
+++ projects/telegram_notification_service/sprint_1/tasks/task_3/solution/size_checker/size_checker/request_check_size.py
@@ -10,10 +10,16 @@ class SizeCheckerException(BaseException):
 
 
 def size_converter(num):
-    prefixes = ['B', 'KiB', 'MiB', 'GiB', 'TiB',
+    suffixes = ['B', 'KiB', 'MiB', 'GiB', 'TiB',
                 'PiB', 'EiB', 'ZiB', 'YiB']
-    recursive_counter = 0
+    # recursive_counter = 0
+    # В варианте функции без рекурсии этой переменной,
+    # пожалуй, здесь не место
 
+    """
+    Оставим это для потомков
+    ~~~~~~~~~~~~~~~~~~~~~~~~
+    
     def calculate_size(content_length):
         nonlocal recursive_counter
         bytes = content_length
@@ -22,16 +28,27 @@ def size_converter(num):
         else:
             recursive_counter += 1
             return calculate_size(bytes / 1024)
-
+            
     size = round(calculate_size(num), 1)
-    if recursive_counter == 0:
-        size = int(size)
-    return f'{size} {prefixes[recursive_counter]}'
+    return f'{size} {suffixes[recursive_counter]}'
+    """
+
+    def calculate_size(content_length):
+        suffix_index = 0
+        result_size = content_length
+        while result_size >= 1024:
+            result_size /= 1024
+            suffix_index += 1
+        return result_size, suffix_index
+
+    calculation_results = calculate_size(num)
+    size = round(calculation_results[0], 1)
+    return f'{size} {suffixes[calculation_results[1]]}'
 
 
 def check_size(url):
     try:
-        response = requests.head(url, timeout=5.0)
+        response = requests.head(url, timeout=5.0, allow_redirects=True)
     except requests.TooManyRedirects:
         raise SizeCheckerException('Sorry, too many redirects')
     except requests.Timeout:
@@ -40,8 +57,7 @@ def check_size(url):
         raise SizeCheckerException('Connection is lost, try again later.')
     else:
         try:
-            response_size = float(response.headers['Content-Length'])
-            return size_converter(response_size)
+            return int(response.headers['Content-Length'])
         except KeyError:
             raise SizeCheckerException('Request is successful, '
                                        'but response is empty')
--- projects/telegram_notification_service/sprint_1/tasks/task_3/solution/size_checker/tests/test_size_checker.py
+++ projects/telegram_notification_service/sprint_1/tasks/task_3/solution/size_checker/tests/test_size_checker.py
@@ -3,9 +3,9 @@ from size_checker.request_check_size import check_size, SizeCheckerException, si
 
 
 def test_size_checker():
-    assert check_size('https://api.github.com/') == '510 B'
-    assert check_size('https://dummyjson.com/todos') == '2.4 KiB'
-    assert check_size('https://httpbin.org/get') == '308 B'
+    assert check_size('https://api.github.com/') == 510
+    assert check_size('https://dummyjson.com/todos') == 2471
+    assert check_size('https://httpbin.org/get') == 308
     print('It\'s ok, boss')
 
 
@@ -15,6 +15,12 @@ def test_size_converter():
     assert size_converter(512) == '512 B'
     assert size_converter(1208925819614629174706176) == '1.0 YiB'
     assert size_converter(0) == '0 B'
+    response_size = check_size('https://api.github.com/')
+    assert size_converter(response_size) == '510 B'
+    response_size = check_size('https://dummyjson.com/todos')
+    assert size_converter(response_size) == '2.4 KiB'
+    response_size = check_size('https://httpbin.org/get')
+    assert size_converter(response_size) == '308 B'
     print('Sizes converted')
 
 
@@ -34,3 +40,9 @@ def test_empty_response():
     with pytest.raises(SizeCheckerException) as err:
         check_size('https://46af-79-101-225-134.ngrok-free.app/timeout_error')
     assert str(err.value) == 'Request is successful, but response is empty'
+
+
+def test_max_redirects():
+    with pytest.raises(SizeCheckerException) as err:
+        check_size('https://2866-79-101-225-134.ngrok-free.app/redirect_error')
+    assert str(err.value) == 'Sorry, too many redirects'
