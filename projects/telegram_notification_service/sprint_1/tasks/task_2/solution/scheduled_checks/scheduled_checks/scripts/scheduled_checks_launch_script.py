ba9e0e9143eb36fbb5cc70408a6df34879aa8d08
Nikita Savin <klaster26@mail.ru>
2024-01-24 02:23:38 +0300

    fix: исправить некорректную работу автоматизатора

--- projects/telegram_notification_service/sprint_1/tasks/task_2/solution/scheduled_checks/scheduled_checks/scripts/scheduled_checks_launch_script.py
+++ projects/telegram_notification_service/sprint_1/tasks/task_2/solution/scheduled_checks/scheduled_checks/scripts/scheduled_checks_launch_script.py
@@ -2,10 +2,8 @@ import scheduled_checks.scheduled_checks as s_c
 import sys
 
 
-def enable_scheduled_checks(path):
+def enable_scheduled_checks():
+    path = sys.argv[1]
     url_list = s_c.get_url_list_from_file(path)
     check_result = s_c.check_urls(url_list)
     s_c.write_check_results_to_file(check_result)
-
-
-enable_scheduled_checks(sys.argv[1])
--- projects/telegram_notification_service/sprint_1/tasks/task_2/solution/scheduled_checks/scheduled_checks/scripts/scheduler.py
+++ projects/telegram_notification_service/sprint_1/tasks/task_2/solution/scheduled_checks/scheduled_checks/scripts/scheduler.py
@@ -2,19 +2,18 @@ from crontab import CronTab
 from getpass import getuser
 import sys
 import os
-'''{os.path.abspath(os.getcwd())}/scheduled_checks_launch_script.py '''
 
 
 def add_to_schedule():
     my_cron = CronTab(user=getuser())
-    job = my_cron.new(command=f'python3 {os.path.abspath(os.getcwd())}/scheduled_checks/'
-                              f'scripts/scheduled_checks_launch_script.py '
-                              f'{sys.argv[1]}')
+    job = my_cron.new(command=f'cd {os.getcwd()}/ && '
+                              f'/home/nikita/.local/bin/poetry run run_check url_list '
+                              f'> {os.getcwd()}/cron_debug.log 2>&1')
     job.minute.every(sys.argv[2])
     my_cron.write()
     if int(sys.argv[2]) > 1:
         minutes = 'minutes'
     else:
         minutes = 'minute'
-    print(f'Auto-checking of sites in {sys.argv[1]} enabled.\n'
+    print(f'Auto-checking of sites in {sys.argv[1]} is enabled.\n'
           f'Sites are being checked every {sys.argv[2]} {minutes}')
